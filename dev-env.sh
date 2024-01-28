9c19cf4b9ddbd226413f48e80951bb5af1f3275f
Sesaque Cruz <cruz.sesaque@gmail.com>
2024-01-28 17:20:35 -0300

    feat: add support to run browser window in docker containers

--- dev-env.sh
+++ dev-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run 'source dev-env.sh' to load env vars during development
+# Run 'source dev-env.sh' to load env vars during development.
 
 export NODE_ENV=development
 
@@ -11,7 +11,7 @@ export EMAIL_SECURE="false"
 export EMAIL_USER="user@maildev.com"
 export EMAIL_PASS="user"
 export SCRAPER_NAVIGATION_TIMEOUT=10000
-export SCRAPER_HEADLESS="false" # Set to false to show the browser window during the scraper operation
+export SCRAPER_HEADLESS="false" # When it is false, the browser window is shown during the scraper operation.
 
 echo "Dev Environment Variables:"
 echo "RABBIT_MQ_URL=$RABBIT_MQ_URL"
